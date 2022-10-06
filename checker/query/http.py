d24ecd8cd9256e29e07661204b6223535185f546
Evan Pratten <ewpratten@gmail.com>
2022-10-05 22:40:59 -0400

    fix some status issues

--- checker/query/http.py
+++ checker/query/http.py
@@ -2,10 +2,11 @@
 import requests
 
 
-def is_host_http_ok(host: str) -> bool:
+def is_host_http_ok(host: str, expect_status=200) -> bool:
     """Check if the host is up and responding to HTTP requests"""
     try:
         response = requests.get(host)
-        return response.status_code == 200
+        print(f"HTTP response: {response.status_code}")
+        return response.status_code == expect_status
     except requests.exceptions.ConnectionError:
         return False
